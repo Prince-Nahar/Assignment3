9e52eecc6a8ae16666319454193184abc2aac49a
tuk86214 <tuk86214@temple.edu>
2024-09-26 12:37:59 -0400

    Added a spinner for the drop down menu and View object to display the colors.

--- app/src/main/java/edu/temple/baseassignment/MainActivity.kt
+++ app/src/main/java/edu/temple/baseassignment/MainActivity.kt
@@ -1,12 +1,18 @@
 package edu.temple.baseassignment
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val colors = arrayOf("Red", "White", "Blue", "Gray", "Purple", "Green", "Yellow", "Magenta", "Silver", "Cyan", "Navy")
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val canvas = findViewById<View>(R.id.colorDisplay)
 
     }
 }
