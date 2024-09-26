d9b424eb072fcb7d08814f03c262d0f85d815626
tuk86214 <tuk86214@temple.edu>
2024-09-26 12:46:44 -0400

    added the ColorAdapter class that extends BaseAdapter

--- app/src/main/java/edu/temple/baseassignment/MainActivity.kt
+++ app/src/main/java/edu/temple/baseassignment/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.temple.baseassignment
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,4 +17,23 @@ class MainActivity : AppCompatActivity() {
         val canvas = findViewById<View>(R.id.colorDisplay)
 
     }
+}
+
+class ColorAdapter : BaseAdapter(){
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
 }
